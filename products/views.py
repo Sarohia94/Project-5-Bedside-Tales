410d686b8d6cb4a97b92e66a407b655ab127f59f
Sarohia94 <sarohia94@gmail.com>
2023-08-24 16:00:07 +0000

    Added product details functionality

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Review
+from .forms import ReviewForm
+
 
 # Create your views here.
 
@@ -7,7 +9,6 @@ def all_products(request):
     """ 
     A view to show all products, including sorting and search queries 
     """
-
     products = Product.objects.all()
 
     context = {
@@ -15,3 +16,20 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ 
+    A view to show individual product details and any reviews left by users
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    review = Review.objects.all().order_by("-date")
+
+    context = {
+        'product': product,
+        'review': review,
+        'review_form': ReviewForm()
+    }
+
+    return render(request, 'products/product_detail.html', context)
+
