d60858e084b360e1d45469fe9227cdf674e889a5
Sarohia94 <sarohia94@gmail.com>
2023-08-26 14:58:18 +0000

    Added additional messages

--- bag/views.py
+++ bag/views.py
@@ -38,13 +38,16 @@ def adjust_bag(request, item_id):
     Adjust the quantity of the specified product to the specified amount
     """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, (f'Updated {product.title} quantity to {bag[item_id]}'))
     else:
         bag.pop(item_id)
+        messages.success(request, (f'Removed {product.title} from your bag'))
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -56,11 +59,14 @@ def remove_from_bag(request, item_id):
     """
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
         bag.pop(item_id)
+        messages.success(request, (f'{product.title} has been from your bag'))
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
