643705b0730c4751544c4a96534b7c62133e4811
Sarohia94 <sarohia94@gmail.com>
2023-08-27 15:21:00 +0000

    Set up wishlist template, model, view and linked urls

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from profiles.models import UserProfile
+from products.models import Product
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    """ 
+    Wishlist model for registered users
+    """
+    user_profile = models.ForeignKey(UserProfile, null=True, blank=True,
+                                     on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=True, blank=True,
+                                on_delete=models.CASCADE)
+    added = models.DateField(auto_now_add=True, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+        
