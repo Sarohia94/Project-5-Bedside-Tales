ded4ed749b33d17b0433a8c44206eb29b792b01f
Sarohia94 <sarohia94@gmail.com>
2023-08-27 21:22:24 +0000

    Secured product admin and profile views

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Product, Review, Category
 from .forms import ReviewForm, ProductForm, AuthorForm
@@ -35,7 +36,6 @@ def all_products(request):
         'current_categories': categories,
     }
 
-
     return render(request, 'products/products.html', context)
 
 
@@ -43,6 +43,7 @@ def product_detail(request, product_id):
     """ 
     A view to show individual product details and any reviews left by users
     """
+
     product = get_object_or_404(Product, pk=product_id)
     review = Review.objects.filter(product=product_id).order_by("-date")
 
@@ -67,10 +68,16 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
 def add_product(request):
     """ 
     Add a product to the store 
     """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -90,6 +97,7 @@ def add_product(request):
     return render(request, template, context)
 
 
+@login_required
 def add_author(request):
     """
     Add an Author to the store 
@@ -122,10 +130,16 @@ def add_author(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_product(request, product_id):
     """ 
     Edit a product in the store 
     """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -148,11 +162,18 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_product(request, product_id):
     """ 
     Delete a product from the store 
     """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
+    
     return redirect(reverse('products'))
--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
-
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
-    """ Display the user's profile. """
+    """ 
+    Display the user's profile
+    """
+
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -36,6 +39,10 @@ def profile(request):
 
 
 def order_history(request, order_number):
+    """
+    Display user's order history
+    """
+    
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
