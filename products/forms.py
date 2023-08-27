694c38bec7cb55c24bc350c4ff81cc2f5098e68d
Sarohia94 <sarohia94@gmail.com>
2023-08-27 21:39:55 +0000

    Add widget for image field for product admin

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from .models import Review, Category, Product, Author
+from .widgets import CustomClearableFileInput
 from django import forms
 from django.contrib.auth.models import User
 
@@ -19,6 +20,8 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+    
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         """
