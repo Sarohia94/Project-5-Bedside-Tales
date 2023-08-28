e72d14c15999290fe9c5830cf8e75d3b72a6575b
Sarohia94 <sarohia94@gmail.com>
2023-08-28 15:48:20 +0000

    Deploying to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bedside_tales.wsgi:application
--- bedside_tales/settings.py
+++ bedside_tales/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-ao(eugkb=^==@fc2jf%z(-_$1swim6@of@@2c$%(klu-hgynjo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-sarohia94-project5bedsi-w6o8qvca7ae.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['project-5-bedside-tales-0c52d874d839.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,13 +125,17 @@ WSGI_APPLICATION = 'bedside_tales.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
